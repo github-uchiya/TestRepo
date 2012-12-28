800caab7436cc20bb4c4345757b8198deaef36ac
github-uchiya <uchiya@gmail.com>
2012-12-28 16:10:02 +0900

    Initial Commit

---
+++ LearningDelegate/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LearningDelegate
+//
+//  Created by UCHIYA on 12/12/28.
+//  Copyright (c) 2012年 H.UCHIYA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
